31a27638950c5dd8ad2280958bb16ce7c3c35d89
Filip Kajetaniak <kajetaniakkfilip@gmail.com>
2019-03-23 23:55:47 +0100

    fixed grid context

--- src/components/admin/NewElement.jsx
+++ src/components/admin/NewElement.jsx
@@ -1,17 +1,26 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import "../../css/NewElement.scss";
-import { elementsContext } from '../../context/elementsContext';
+import { elementsContext } from "../../context/elementsContext";
 
 export default function NewElement({ data }) {
-  const { tescik } = useContext(elementsContext);
-  useEffect(() => {
-    console.log('stworzono nowy element')
-    return () => {console.log('usunięto element')}
-  })
+  const { elements, setElements } = useContext(elementsContext);
+  const addElement = () => {
+    setElements([
+      ...elements,
+      {
+        left: `${data.left}%`,
+        top: `${data.top}px`,
+        width: `${data.width}%`,
+        height: `${data.height}px`,
+        id: new Date().getTime() + Math.floor(Math.random() * 9999999 + 1111111)
+      }
+    ]);
+  };
   return (
     data && (
       <div
         className="new-element"
+        onClick={addElement}
         style={
           data.top || data.left || data.width || data.height
             ? {
--- src/containers/GridContainer.jsx
+++ src/containers/GridContainer.jsx
@@ -1,13 +1,16 @@
-import React, {useState} from 'react';
-import { elementsContext } from '../context/elementsContext';
+import React, { useState, useEffect } from "react";
+import { elementsContext } from "../context/elementsContext";
 
-export default function GridContainer({children}) {
-    const [elements, setElements] = useState({dfdf:'dfdf'});
-    return (
-        <>
-            <elementsContext.Provider value={{elements, setElements}}>
-                {children}
-            </elementsContext.Provider>
-        </>
-    )
+export default function GridContainer({ children }) {
+  const [elements, setElements] = useState([]);
+  useEffect(() => {
+    console.log(elements);
+  });
+  return (
+    <>
+      <elementsContext.Provider value={{ elements, setElements }}>
+        {children}
+      </elementsContext.Provider>
+    </>
+  );
 }
