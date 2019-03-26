fae5cf09cd090211d321a2e38d95ca1db5c3416d
Filip Kajetaniak <kajetaniakkfilip@gmail.com>
2019-03-26 19:54:35 +0100

    minor fixes

--- src/components/admin/MovingHandle.jsx
+++ src/components/admin/MovingHandle.jsx
@@ -7,7 +7,7 @@ import { gridContext } from "../../context/gridContext";
 export default function MovingHandle({ currentParameters }) {
   const { setNewElementData } = useContext(newElementContext);
   const { gridParameters } = useContext(gridContext);
-  const [cursorOffset, setCursorOffset] = useState({ top: 0, left: 0 });
+  const [topOffset, setTopOffset] = useState(0);
   const [isMoving, setIsMoving] = useState(false);
   const { margin, gridWidth } = getMarginWidth(gridParameters.width);
   const movingHandleDiv = useRef();
@@ -17,10 +17,9 @@ export default function MovingHandle({ currentParameters }) {
       ref={movingHandleDiv}
       className={isMoving ? "moving-handle moving" : "moving-handle"}
       onMouseDown={e => {
-        setCursorOffset({
-          top: e.clientY - movingHandleDiv.current.parentElement.offsetTop,
-          left: movingHandleDiv.current.parentElement.offsetLeft
-        });
+        setTopOffset(
+          e.clientY - movingHandleDiv.current.parentElement.offsetTop
+        );
         setIsMoving(true);
       }}
       onMouseUp={() => setIsMoving(false)}
@@ -31,10 +30,8 @@ export default function MovingHandle({ currentParameters }) {
         }
         setNewElementData({
           ...currentParameters,
-          top: e.clientY - cursorOffset.top,
-          left:
-            Math.round(((e.clientX - margin) / gridWidth) * 100 * 10) / 10 -
-            cursorOffset.left
+          top: e.clientY - topOffset,
+          left: Math.round(((e.clientX - margin) / gridWidth) * 100 * 10) / 10
         });
       }}
     />
