5304d98159bd400c723985cf11a5d996b8ee3977
Filip Kajetaniak <kajetaniakkfilip@gmail.com>
2019-04-01 21:17:46 +0200

    implemented grid resizing

--- src/components/admin/GridResizeHandles.jsx
+++ src/components/admin/GridResizeHandles.jsx
@@ -3,70 +3,66 @@ import "../../css/GridResizeHandles.scss";
 import { gridContext } from "../../context/gridContext";
 
 export default function GridResizeHandles() {
-  const [resizingLeft, setResizingLeft] = useState(false);
-  const [resizingRight, setResizingRight] = useState(false);
+  const [resizing, setResizing] = useState({ left: false, right: false });
   const { gridParameters, setGridParameters } = useContext(gridContext);
-  const resizeLeft = e => {
-    if (!resizingLeft) {
+  const windowWidth = document.documentElement.clientWidth;
+  const resize = (e, side) => {
+    if (!resizing[side]) {
       return;
     }
-    console.log("RESIZING LEFT");
+    const margin = side === "right" ? windowWidth - e.clientX : e.clientX;
+    const gridWidth = windowWidth - margin * 2;
+    setGridParameters({
+      ...gridParameters,
+      width: gridWidth < 50 ? 50 : gridWidth
+    });
   };
-  const resizeRight = e => {
-    if (!resizingRight) {
-      return;
-    }
-    console.log("RESIZING RIGHT");
+  const startResizing = (e, side) => {
+    e.persist();
+    setResizing({
+      ...resizing,
+      [side]: true
+    });
+    setGridParameters({
+      ...gridParameters,
+      canSelect: false
+    });
+  };
+  const stopResizing = (e, side) => {
+    e.persist();
+    setResizing({
+      ...resizing,
+      [side]: false
+    });
+    setGridParameters({
+      ...gridParameters,
+      canSelect: true
+    });
   };
   return (
-    <>
-      <div
-        className={
-          resizingLeft
-            ? "grid-resize-handle grid-handle-left resizing"
-            : "grid-resize-handle grid-handle-left"
-        }
-        onMouseDown={e => {
-          e.persist();
-          setGridParameters({
-            ...gridParameters,
-            canSelect: false
-          });
-          console.log("GRID RESIZER");
-          setResizingLeft(true);
-        }}
-        onMouseMove={resizeLeft}
-        onMouseUp={e => {
-          setResizingLeft(false);
-          setGridParameters({
-            ...gridParameters,
-            canSelect: true
-          });
-        }}
-      />
-      <div
-        className={
-          resizingRight
-            ? "grid-resize-handle grid-handle-right resizing"
-            : "grid-resize-handle grid-handle-right"
-        }
-        onMouseDown={e => {
-          e.persist();
-          setGridParameters({
-            ...gridParameters,
-            canSelect: false
-          });
-          setResizingRight(true);
-        }}
-        onMouseMove={resizeRight}
-        onMouseUp={e => {
-          setGridParameters({
-            ...gridParameters,
-            canSelect: true
-          });
-          setResizingRight(false);
-        }}
-      />
-    </>
+    windowWidth > gridParameters.width && (
+      <>
+        <div
+          className={
+            resizing.left
+              ? "grid-resize-handle grid-handle-left resizing"
+              : "grid-resize-handle grid-handle-left"
+          }
+          onMouseDown={e => startResizing(e, "left")}
+          onMouseMove={e => resize(e, "left")}
+          onMouseUp={e => stopResizing(e, "left")}
+        />
+        <div
+          className={
+            resizing.right
+              ? "grid-resize-handle grid-handle-right resizing"
+              : "grid-resize-handle grid-handle-right"
+          }
+          onMouseDown={e => startResizing(e, "right")}
+          onMouseMove={e => resize(e, "right")}
+          onMouseUp={e => stopResizing(e, "right")}
+        />
+      </>
+    )
   );
 }
